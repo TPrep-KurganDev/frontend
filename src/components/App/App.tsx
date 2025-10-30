1823de5db174bcbe1a69b0c587e10a520d9c1e34
yura <yura.romashov000@mail.ru>
2025-10-31 01:33:22 +0500

    сделал FavoriteExamListScreen

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -5,6 +5,7 @@ import {BrowserRouter, Route, Routes} from 'react-router-dom';
 import {AppRoute} from '../../const.ts';
 import {CardEditScreen} from '../../pages/CardEditScreen/CardEditScreen.tsx';
 import {ExamListScreen} from '../../pages/ExamListScreen/ExamListScreen.tsx';
+import {FavoriteExamListScreen} from '../../pages/FavoriteExamScreen/FavoriteExamScreen.tsx';
 
 export default function App() {
   return (
@@ -20,12 +21,16 @@ export default function App() {
             element={<CardScreen/>}
           />
           <Route
-            path={'/cardEdit'}
+            path={'/card-edit'}
             element={<CardEditScreen/>}
           />
           <Route
-            path={'/examList'}
+            path={'/exam-list'}
             element={<ExamListScreen/>}
+          />
+          <Route
+            path={'/favorite-exam-list'}
+            element={<FavoriteExamListScreen/>}
           />
         </Routes>
       </BrowserRouter>
