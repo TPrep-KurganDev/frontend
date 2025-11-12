3b4b53b0f36904dd51f8736339249faba01eb7aa
belozerov-yaroslav <jarblz@yandex.ru>
2025-11-13 02:32:07 +0500

    back в header

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -8,9 +8,13 @@ type HeaderProps = {
 }
 
 export default function Header({ title, imgSrc, widthImg, heightImg }: HeaderProps) {
+  const handleBack = () => {
+    window.history.back();
+  };
+
   return (
     <div className={styles.header}>
-      <img src="Arrow 1.svg" className={styles.backButton} width="23" height="23" alt='backButton'/>
+      <img src="Arrow 1.svg" className={styles.backButton} width="23" height="23" alt='backButton' onClick={handleBack}/>
       <h1 className={styles.title}>{title}</h1>
       {imgSrc && <img src={imgSrc} className={styles.settingsButton} width={widthImg} height={heightImg} alt='settingsButton'/>}
     </div>
