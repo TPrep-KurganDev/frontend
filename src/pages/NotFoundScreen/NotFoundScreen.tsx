d125772ed6da538de7b5f6787c77e0de70819af3
yura <yura.romashov000@mail.ru>
2025-12-16 15:25:11 +0500

    сверстал страничку с ошибкой

--- src/pages/NotFoundScreen/NotFoundScreen.tsx
+++ src/pages/NotFoundScreen/NotFoundScreen.tsx
@@ -1,7 +1,17 @@
+import Header from '../../components/Header/Header';
+import styles from './NotFoundScreen.module.scss';
+
 function NotFoundScreen() {
-  return <div>
-    404
-  </div>
+  return (
+    <>
+      <Header title={''} inputDisabled={true} inputRef={undefined} onInputBlur={() => {}} onTitleChange={()=>{}} backButtonPage={'/'}/>
+      <div className={styles.error}>
+        <div className={styles.numberError}>404</div>
+        <div className={styles.header}>Такой страницы нет</div>
+        <div className={styles.text}>Но есть много других полезных страницы</div>
+      </div>
+    </>
+  )
 }
 
 export default NotFoundScreen
